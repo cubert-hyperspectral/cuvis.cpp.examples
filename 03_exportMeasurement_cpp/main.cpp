24d2d70b63d40e2d10a554e921e246dbb33562bc
Luca <schmid@cubert-gmbh.de>
2026-03-24 11:45:10 +0100

    fix memory error in the examples after shutdown, added comments, Updated CMake Solution Name

--- 03_exportMeasurement_cpp/main.cpp
+++ 03_exportMeasurement_cpp/main.cpp
@@ -6,104 +6,185 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc != 5)
-  {
-    std::cout << std::endl << "Too few Arguments! Please provide:" << std::endl;
-    std::cout << "user settings directory" << std::endl;
-    std::cout << "sessionfile (.cu3s)" << std::endl;
-    std::cout << "user plugin file (.xml)" << std::endl;
-    std::cout << "Name of export directory" << std::endl;
+    {
+        /*
+        * Expot / Convert Cubert Measurements to different file formats
+        * 
+        * This example provides information on the exporter classes and the file formats which they can convert measurements to.
+        *
+        * Used principles:
+        *   - *SessionFile* as a source for measurements
+        *   - *CubeExporter* for saving measurements
+        *   - *TiffExporter* for exporting to TIFF format
+        *   - *EnviExporter* for exporting to ENVI format
+        *   - *ViewExporter* for exporting rendered views of the data
+        *   - *UserPlugins* to define how a view is computed
+        *
+        * Prerequisites to running this example:
+        *   - Have recorded a *SessionFile* (.cu3s) *or* downloaded the provided [demo data](https://drive.google.com/drive/folders/1Cjb0v_a2p1cCmhKH8w2OuRtnhXCJGz61?usp=sharing)
+        *   - Have the Cuvis SDK installed
+        *
+        * Run properties
+        *   - "path/to/settings" path/to/measurement/single.cu3s" "path/to/user/plugin/file.xml" "OutputFolderName"
+        */
+        if (argc != 5)
+        {
+            std::cout << std::endl << "Too few Arguments! Please provide:" << std::endl;
+            std::cout << "user settings directory" << std::endl;
+            std::cout << "sessionfile (.cu3s)" << std::endl;
+            std::cout << "user plugin file (.xml)" << std::endl;
+            std::cout << "Name of export directory" << std::endl;
 
-    return -1;
-  }
-  char* const userSettingsDir = argv[1];
-  char* const sessionLoc = argv[2];
-  char* const pluginLoc = argv[3];
-  char* const exportDir = argv[4];
+            return -1;
+        }
+        char* const userSettingsDir = argv[1];
+        char* const sessionLoc = argv[2];
+        char* const pluginLoc = argv[3];
+        char* const exportDir = argv[4];
 
-  std::cout << "Example 03 export measurement" << std::endl;
-  std::cout << "User Settings Dir: " << userSettingsDir << std::endl;
-  std::cout << "sessionfile (.cu3s): " << sessionLoc << std::endl;
-  std::cout << "user plugin file (.xml): " << pluginLoc << std::endl;
-  std::cout << "Export Dir: " << exportDir << std::endl;
+        std::cout << "Example 03 export measurement" << std::endl;
+        std::cout << "User Settings Dir: " << userSettingsDir << std::endl;
+        std::cout << "sessionfile (.cu3s): " << sessionLoc << std::endl;
+        std::cout << "user plugin file (.xml): " << pluginLoc << std::endl;
+        std::cout << "Export Dir: " << exportDir << std::endl;
 
-  std::cout << "loading settings... " << std::endl;
-  cuvis::General::init(userSettingsDir);
-  cuvis::General::set_log_level(loglevel_info);
+        std::cout << "loading settings... " << std::endl;
+        cuvis::General::init(userSettingsDir);
+        cuvis::General::set_log_level(loglevel_info);
 
-  std::cout << "loading session... " << std::endl;
-  cuvis::SessionFile sess(sessionLoc);
+        std::cout << "loading session... " << std::endl;
+        cuvis::SessionFile sess(sessionLoc);
 
-  std::cout << "loading measurement... " << std::endl;
-  auto optmesu = sess.get_mesu(0);
-  assert(optmesu.has_value());
-  cuvis::Measurement mesu = optmesu.value();
+        std::cout << "loading measurement... " << std::endl;
+        auto optmesu = sess.get_mesu(0);
+        assert(optmesu.has_value());
+        cuvis::Measurement mesu = optmesu.value();
 
-  assert(mesu.get_meta()->processing_mode != cuvis::processing_mode_t::Preview);
+        assert(mesu.get_meta()->processing_mode != cuvis::processing_mode_t::Preview);
 
-  {
-    std::cout << "Export to Envi" << std::endl;
-    cuvis::EnviArgs args;
-    char exportDirEnvi[CUVIS_MAXBUF];
-    strcpy(exportDirEnvi, exportDir);
-    strcat(exportDirEnvi, "/envi");
-    args.export_dir = exportDirEnvi;
-    cuvis::EnviExporter exporter(args);
-    exporter.apply(mesu);
-  }
-  {
-    std::cout << "Export to Multi-Channel Tiff" << std::endl;
-    cuvis::TiffArgs args;
-    char exportDirMulti[CUVIS_MAXBUF];
-    strcpy(exportDirMulti, exportDir);
-    strcat(exportDirMulti, "/multi");
-    args.export_dir = exportDirMulti;
-    args.format = cuvis::tiff_format_t::tiff_format_MultiChannel;
-    cuvis::TiffExporter exporter(args);
-    exporter.apply(mesu);
-  }
-  {
-    std::cout << "Export to separate Tiffs" << std::endl;
-    cuvis::TiffArgs args;
-    char exportDirSingle[CUVIS_MAXBUF];
-    strcpy(exportDirSingle, exportDir);
-    strcat(exportDirSingle, "/single");
-    args.export_dir = exportDirSingle;
-    args.format = cuvis::tiff_format_t::tiff_format_Single;
-    cuvis::TiffExporter exporter(args);
-    exporter.apply(mesu);
-  }
-  {
-    std::cout << "Export View to file" << std::endl;
-    cuvis::ViewArgs args;
-    char exportDirView[CUVIS_MAXBUF];
-    strcpy(exportDirView, exportDir);
-    strcat(exportDirView, "/view");
-    args.export_dir = exportDirView;
+        {
+            /*
+            * ENVI Exporter
+            * For the *EnviExporter* only some basic settings are available.
+            * This exporter will create two files per measurement: A `.hdr` file and a data file with the same name but without a file extension.
+            * `export_dir` and `channel_selection` apply the same as for *TiffExportSettings* above.
+            */
+            std::cout << "Export to Envi" << std::endl;
+            cuvis::EnviArgs args;
+            char exportDirEnvi[CUVIS_MAXBUF];
+            strcpy(exportDirEnvi, exportDir);
+            strcat(exportDirEnvi, "/envi");
+            args.export_dir = exportDirEnvi;
+            cuvis::EnviExporter exporter(args);
+            exporter.apply(mesu);
+        }
+        {
+            /*
+            * TIFF Exporter
+            * #### TIFF Exporter
+            * The *TiffExporter* is used to export hyperspectral cubes (or parts thereof) in *.tiff* (*.tif*) format. As the TIFF format is fairly flexible, three general modes are available:
+            *   - MultiChannel: The cube is stored as a single image in a single file with each wavelength band as a seperate channel
+            *   - MultiPage: The cube is stored as multiple single channel (monochrome) images in a single file. Each wavelength band is a separate "page" within the TIFF file.
+            *   - Single: The cube is stored as a multiple single channel (monochrome) image in multiple files. Each wavelength band is a separate TIFF file.
+            *
+            * The TIFF export mode is set via the *TiffExportSettings* class when creating the *TiffExporter*. Here is an overview of further useful settings:
+            *   - `export_dir`: The directory to export the measurements to
+            *   - `channel_selection`: Select which wavelength bands are included in the export by their wavelength \[nm\] value. This is set via string using a selection syntax. Here are some valid examples:
+            *   - Include only the channel at 400nm: "400"
+            *   - Include channels at 400, 410 and 620nm: "400;410;620"
+            *   - Include all channels from 400 to 500nm (both exclusive): "400-500"
+            *   - Include the channels 400 to 500nm and channel at 600nm: "400-500;600"
+            *   - Include all channels: "all"
+            *   - Include the channels between 400 and 600nm in 20nm steps: "400:20:600"
+            *   - `compression_mode`: Which compression scheme to use. To enable compression, set to `TiffCompressionMode.LZW`.
+            *   - `format`: Which TIFF export mode to use. Default: `TiffFormat.MultiChannel`
+            */
+            std::cout << "Export to Multi-Channel Tiff" << std::endl;
+            cuvis::TiffArgs args;
+            char exportDirMulti[CUVIS_MAXBUF];
+            strcpy(exportDirMulti, exportDir);
+            strcat(exportDirMulti, "/multi");
+            args.export_dir = exportDirMulti;
+            args.format = cuvis::tiff_format_t::tiff_format_MultiChannel;
+            cuvis::TiffExporter exporter(args);
+            exporter.apply(mesu);
+        }
+        {
+            std::cout << "Export to separate Tiffs" << std::endl;
+            cuvis::TiffArgs args;
+            char exportDirSingle[CUVIS_MAXBUF];
+            strcpy(exportDirSingle, exportDir);
+            strcat(exportDirSingle, "/single");
+            args.export_dir = exportDirSingle;
+            args.format = cuvis::tiff_format_t::tiff_format_Single;
+            cuvis::TiffExporter exporter(args);
+            exporter.apply(mesu);
+        }
+        {
+            /*
+            * View Exporter
+            * The *ViewExporter* enables rendering and exporting views of the cube data, ie. RGB, Color Infrared, or indices like NDVI.
+            * The views are always RGB images.
+            * How the view is rendered / computed, is described in using a *UserPlugin* - an XML file with a special syntax that describes data accesses and mathematical operations used to compute the view.
+            * These files are described in more detail below.
+            *
+            * The *ViewExporter* is initialized with the class *ViewExportSettings*, like the other exporters.
+            * Here is an overview of some useful settings:
+            *    - `export_dir` and `channel_selection`: Same as for *TiffExportSettings* above
+            *    - `userplugin`: Either the path to the *UserPlugin* `.xml` file or a valid XML string of a *UserPlugin*
+            *    - `pan_failback`: Controls the behavior if no cube is available. If `True`, allows using the panchromatic or preview image to be used as a fallback output. Else, throws an exception instead.
+            *
+            * User Plugins
+            * The *UserPlugin* is a XML schema definition for how a hyperspectral cube is accessed and processed to compute a regular RGB image from it.
+            * You can find the XML schema in your Cuvis installation under `Cuvis/user/plugin/userplugin.xsd`
+            *
+            * A selection of plugins is provided with your installation of Cuvis under `Cuvis/user/plugin`. The plugins differ slightly between processing modes, as some may only be applicable to, e.g. reflectance data.
+            * For further information on *UserPlugins*, their syntax and the available operators, please refer to the *Cuvis User Plug-Ins manual* PDF included with your Cuvis installation.
+            */
+            std::cout << "Export View to file" << std::endl;
+            cuvis::ViewArgs args;
+            char exportDirView[CUVIS_MAXBUF];
+            strcpy(exportDirView, exportDir);
+            strcat(exportDirView, "/view");
+            args.export_dir = exportDirView;
 
-    std::cout << "Load plugin" << std::endl;
-    std::ifstream file(pluginLoc);
-    args.userplugin = std::string(
-        (std::istreambuf_iterator<char>(file)),
-        std::istreambuf_iterator<char>());
+            std::cout << "Load plugin" << std::endl;
+            std::ifstream file(pluginLoc);
+            args.userplugin = std::string(
+                (std::istreambuf_iterator<char>(file)),
+                std::istreambuf_iterator<char>());
 
-    cuvis::ViewExporter exporter(args);
-    exporter.apply(mesu);
-  }
-  {
-    std::cout << "Export to sessionfile" << std::endl;
-    char exportDirView[CUVIS_MAXBUF];
-    strcpy(exportDirView, exportDir);
-    strcat(exportDirView, "/session");
+            cuvis::ViewExporter exporter(args);
+            exporter.apply(mesu);
+        }
+        {
+            /*
+            * Cube Exporter - Settings (SaveArgs)
+            * When setting up a *CubeExporter* there are many settings to fine-tune the export process and define what gets saved.
+            * The *SaveArgs* class is used to communicate the settings to the *CubeExporter*.
+            * 
+            * Here is an overview of the most important attributes of *SaveArgs*:
+            *    - `export_dir`: The directory to export the measurements to
+            *    - `allow_overwrite`: Allow overwriting files in case of a name clash
+            *    - `allow_fragmentation`: Start a new *SessionFile* for each measurement / Allow only one measurement per *SessionFile*
+            *    - `allow_drop`: Allow the exporter to drop measurements if it cannot write to the disk fast enough
+            *    - `allow_info_file`: Create an info file alongside the *SessionFile*. This file contains a list of all measurements in the *SessionFile* and also marks dropped measurements
+            *    - `full_export`: Include the hyperspectral cube when saving to disk. This is **FALSE** by default! *Please note:* The cube can **always be recomputed** on demand from the data stored in the *SessionFile*. Saving the cube can make sense to speed up or allow access to cube data on systems with low or insufficient processing power. It does significantly increase the size of the *SessionFiles*, usually roughly 2x. Additionally, if the cube is included, the processing mode (RAW, Reflectance, ...) is preserved.
+            */
+            std::cout << "Export to sessionfile" << std::endl;
+            char exportDirView[CUVIS_MAXBUF];
+            strcpy(exportDirView, exportDir);
+            strcat(exportDirView, "/session");
 
-    cuvis::SaveArgs saveArgs;
-    saveArgs.export_dir = exportDirView;
-    saveArgs.allow_overwrite = true;
-    saveArgs.allow_session_file = true;
+            cuvis::SaveArgs saveArgs;
+            saveArgs.export_dir = exportDirView;
+            saveArgs.allow_overwrite = true;
+            saveArgs.allow_session_file = true;
 
-    cuvis::CubeExporter exporter(saveArgs);
-    exporter.apply(mesu);
-  }
+            cuvis::CubeExporter exporter(saveArgs);
+            exporter.apply(mesu);
+        }
+    }
   cuvis::General::shutdown();
   std::cout << "finished." << std::endl;
 }
--- 05_recordSingleImages_cpp/main.cpp
+++ 05_recordSingleImages_cpp/main.cpp
@@ -8,122 +8,170 @@ namespace fs = std::filesystem;
 
 int main(int argc, char* argv[])
 {
-  if (argc != 6)
-  {
-    std::cout << std::endl << "Too few Arguments! Please provide:" << std::endl;
-    std::cout << "user settings directory  or .cu3c file" << std::endl;
-    std::cout << "factory directory" << std::endl;
-    std::cout << "name of recording directory" << std::endl;
-    std::cout << "exposure time in ms" << std::endl;
-    std::cout << "number of images" << std::endl;
-
-    return -1;
-  }
-
-  char* const userSettingsDir = argv[1];
-  fs::path factoryDir(argv[2]);
-  char* const recDir = argv[3];
-  char* const exposureString = argv[4];
-  char* const nrImagesString = argv[5];
-
-  int exposure_ms = std::stoi(exposureString);
-  int nrImages = std::stoi(nrImagesString);
-
-  std::cout << "Example 05 record single image " << std::endl;
-  std::cout << "User Settings Dir: " << userSettingsDir << std::endl;
-  std::cout << "Factory Dir or .cu3c file: " << factoryDir.string() << std::endl;
-  std::cout << "Recording Dir: " << recDir << std::endl;
-  std::cout << "Exposure in ms: " << exposure_ms << std::endl;
-  std::cout << "Number of Images: " << nrImages << std::endl;
-
-  std::cout << "loading user settings..." << std::endl;
-  cuvis::General::init(userSettingsDir);
-  cuvis::General::set_log_level(loglevel_info);
-
-  std::cout << "Loading Calibration and processing context..." << std::endl;
-
-  const auto  get_calib = [factoryDir]()
     {
-      if (fs::is_directory(factoryDir))
-      {
-        return  cuvis::Calibration(factoryDir);
-      }
-      else if (fs::is_regular_file(factoryDir) && factoryDir.extension() == ".cu3c") {
-        std::cout << " using .cu3c file as calibration instead of factory dir..." << std::endl;
-        cuvis::SessionFile calibFile(factoryDir);
-        return cuvis::Calibration(calibFile);
-      }
-      else {
-        throw std::invalid_argument("Unrecognized file format.");
-      }
-    };
+        /*
+        * Connect to camera and record a measurement
+        *
+        * This example provides a minimal starting point to allow you to get a camera and data acquisition running.
+        *
+        * Used principles:
+        *   - *AcquisitionContext* for camera control and data acquisition
+        *   - *SessionFile* as camera calibration file
+        *   - *CubeExporter* for saving measurements
+        *
+        * Prerequisites to running this example:
+        *   - Have a camera connected *or* downloaded the provided [demo data](https://drive.google.com/drive/folders/1Cjb0v_a2p1cCmhKH8w2OuRtnhXCJGz61?usp=sharing)
+        *   - Have the camera calibration file (*SN*.cu3c) ready *or* use the [demo data](https://drive.google.com/drive/folders/1Cjb0v_a2p1cCmhKH8w2OuRtnhXCJGz61?usp=sharing)
+        *   - Have the Cuvis SDK installed
+        *
+        * Run properties:
+        *   - "path/to/settings" path/to/factory" "path/to/recording/directory" exPoseTimeInMs numberOfImages
+        */
+        if (argc != 6)
+        {
+            std::cout << std::endl << "Too few Arguments! Please provide:" << std::endl;
+            std::cout << "user settings directory  or .cu3c file" << std::endl;
+            std::cout << "factory directory" << std::endl;
+            std::cout << "name of recording directory" << std::endl;
+            std::cout << "exposure time in ms" << std::endl;
+            std::cout << "number of images" << std::endl;
 
-  cuvis::Calibration calib = get_calib();
+            return -1;
+        }
+
+        char* const userSettingsDir = argv[1];
+        fs::path factoryDir(argv[2]);
+        char* const recDir = argv[3];
+        char* const exposureString = argv[4];
+        char* const nrImagesString = argv[5];
+
+        int exposure_ms = std::stoi(exposureString);
+        int nrImages = std::stoi(nrImagesString);
+
+        std::cout << "Example 05 record single image " << std::endl;
+        std::cout << "User Settings Dir: " << userSettingsDir << std::endl;
+        std::cout << "Factory Dir or .cu3c file: " << factoryDir.string() << std::endl;
+        std::cout << "Recording Dir: " << recDir << std::endl;
+        std::cout << "Exposure in ms: " << exposure_ms << std::endl;
+        std::cout << "Number of Images: " << nrImages << std::endl;
+
+        /*
+        * Initialize the Cuvis SDK using a settings - directory
+        * This is optional(all settings have defaults),
+        * but enables you to optimize Cuvis' performance on your system using the settings
+        * Your camera and the default Cuvis installation both provide these settings files
+        */
+        std::cout << "loading user settings..." << std::endl;
+        cuvis::General::init(userSettingsDir);
+        cuvis::General::set_log_level(loglevel_info);
+
+        std::cout << "Loading Calibration and processing context..." << std::endl;
+
+        const auto  get_calib = [factoryDir]()
+            {
+                if (fs::is_directory(factoryDir))
+                {
+                    return  cuvis::Calibration(factoryDir);
+                }
+                else if (fs::is_regular_file(factoryDir) && factoryDir.extension() == ".cu3c") {
+                    std::cout << " using .cu3c file as calibration instead of factory dir..." << std::endl;
+                    cuvis::SessionFile calibFile(factoryDir);
+                    return cuvis::Calibration(calibFile);
+                }
+                else {
+                    throw std::invalid_argument("Unrecognized file format.");
+                }
+            };
+
+        cuvis::Calibration calib = get_calib();
 
 
-  cuvis::ProcessingContext proc(calib);
-  cuvis::AcquisitionContext acq(calib);
+        cuvis::ProcessingContext proc(calib);
+        /*
+        * Acquisition Context
+        * The* Acquisition Context* is your interface to control the camera and all aspects of the data acquisition.
+        *
+        * Initialize it using a* SessionFile* object, then set the recording parameters and start an acquisition.
+        * As soon as the** AcquisitionContext** is created, it will try to establish a connection with the camera.
+        *
+        * Here, the "Software" operation mode is used to enable data acquisition using a software trigger.
+        * This is also called snapshot mode.
+        *
+        * Please note :
+        * The* AcquisitionContext* will** only** connect to the** exact** camera of the same serial number matching the calibration file!
+        * All other cameras / devices are ignored.
+        */
+        cuvis::AcquisitionContext acq(calib);
 
-  cuvis::SaveArgs saveArgs;
-  saveArgs.allow_overwrite = true;
-  saveArgs.export_dir = recDir;
-  saveArgs.allow_session_file = true;
+        cuvis::SaveArgs saveArgs;
+        saveArgs.allow_overwrite = true;
+        saveArgs.export_dir = recDir;
+        saveArgs.allow_session_file = true;
 
-  cuvis::CubeExporter exporter(saveArgs);
+        // Setup the Cube Exporter for saving the measurements to disk in the SessionFile format(.cu3s)
+        cuvis::CubeExporter exporter(saveArgs);
 
-  while (cuvis::hardware_state_t::hardware_state_offline == acq.get_state())
-  {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "Waiting for camera to come online ..." << std::endl;
-  }
+        while (cuvis::hardware_state_t::hardware_state_offline == acq.get_state())
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::cout << "Waiting for camera to come online ..." << std::endl;
+        }
 
-  std::cout << "Camera is online" << std::endl;
-  acq.set_operation_mode(cuvis::operation_mode_t::OperationMode_Software).get();
-  acq.set_integration_time(exposure_ms).get();
+        std::cout << "Camera is online" << std::endl;
+        acq.set_operation_mode(cuvis::operation_mode_t::OperationMode_Software).get();
+        acq.set_integration_time(exposure_ms).get();
 
-  std::cout << "Start recording now" << std::endl;
-  for (int k = 0; k < nrImages; k++)
-  {
-    std::cout << "Record image #" << k << "... (async) ";
-    auto async_mesu = acq.capture();
-    auto mesu_res = async_mesu.get(std::chrono::milliseconds(500));
-    if (mesu_res.first == cuvis::async_result_t::done &&
-      mesu_res.second.has_value())
-    {
-      auto& mesu = mesu_res.second.value();
+        /*
+        * Capturing a Measurement with Software Trigger(Single Snapshot)
+        * Using the `capture()` method, a single measurement is initiated.
+        * Taking a snapshot requires some time, so, to prevent the call to `capture()` from blocking execution, an* AsyncMesu* is returned.
+        * To await the completion of the snapshot, use the `get()` method on the *AsyncMesu*.
+        */
+        std::cout << "Start recording now" << std::endl;
+        for (int k = 0; k < nrImages; k++)
+        {
+            std::cout << "Record image #" << k << "... (async) ";
+            auto async_mesu = acq.capture();
+            auto mesu_res = async_mesu.get(std::chrono::milliseconds(500));
+            if (mesu_res.first == cuvis::async_result_t::done &&
+                mesu_res.second.has_value())
+            {
+                auto& mesu = mesu_res.second.value();
 
-      proc.apply(mesu);
-      exporter.apply(mesu);
+                proc.apply(mesu);
+                exporter.apply(mesu);
 
-      std::cout << "done" << std::endl;
+                std::cout << "done" << std::endl;
+            }
+            else
+            {
+                std::cout << "failed" << std::endl;
+            }
+        }
+
+        //uncomment for recording in queue mode
+        /*
+        for (int k = 0; k < nrImages; k++)
+        {
+          std::cout << "Record image #" << k << "... (queue)";
+          acq.capture_queue();
+          auto mesu = acq.get_next_measurement(std::chrono::milliseconds(500));
+          if (mesu)
+          {
+            proc.apply(mesu.value());
+            exporter.apply(mesu.value());
+            std::cout << "done" << std::endl;
+          }
+          else
+          {
+            std::cout << "failed" << std::endl;
+          }
+          std::cout << "done" << std::endl;
+        }
+        */
     }
-    else
-    {
-      std::cout << "failed" << std::endl;
-    }
-  }
-
-  //uncomment for recording in queue mode
-  /*
-  for (int k = 0; k < nrImages; k++)
-  {
-    std::cout << "Record image #" << k << "... (queue)";
-    acq.capture_queue();
-    auto mesu = acq.get_next_measurement(std::chrono::milliseconds(500));
-    if (mesu)
-    {
-      proc.apply(mesu.value());
-      exporter.apply(mesu.value());
-      std::cout << "done" << std::endl;
-    }
-    else
-    {
-      std::cout << "failed" << std::endl;
-    }
-    std::cout << "done" << std::endl;
-  }
-  */
 
   cuvis::General::shutdown();
   std::cout << "finished." << std::endl;
+  return 0;
 }
